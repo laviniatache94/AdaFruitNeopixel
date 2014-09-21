684ebe0b1d1043cdc88783ee3a898f55b70806d0
laviniatache94 <tache.lavinia@yahoo.ro>
2014-09-21 21:46:41 +0300

    sdgsg

--- firmware/lavinia.h
+++ firmware/lavinia.h
@@ -19,9 +19,7 @@
 #ifndef ADAFRUIT_NEOPIXEL_H
 #define ADAFRUIT_NEOPIXEL_H
 
-#ifdef SPARK_CORE
-  #include "application.h"
-#endif
+#include "application.h"
 
 // 'type' flags for LED pixels (third parameter to constructor):
 #define NEO_RGB     0x00 // Wired for RGB data order
